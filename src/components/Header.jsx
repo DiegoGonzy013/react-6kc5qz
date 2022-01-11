f16bb478474c5e4ec197321375e02725250b1d4b
DiegoGonzy013 <boria37diego@gmail.com>
2022-01-11 11:37:33 +0100

    1 file modified, 1 added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import React from 'react';
 import { Link } from 'react-router-dom';
+import {MenuItems} from'../data/MenuItems';
 import './Headerstyle.css';
 
 class Header extends React.Component {
